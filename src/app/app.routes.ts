20e71d55e0a3daf13c4d3a88d76d8bd55b161825
burdu <23202223@ibsu.edu.ge>
2026-03-21 00:43:51 +0400

    fix(main): implement favicon and robots

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,22 +3,33 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
+    title: 'Opifx | Image Editor',
     loadComponent: () =>
       import('./features/editor/container/editor-container').then((c) => c.EditorContainer),
     children: [
       {
         path: '',
-        loadComponent: () => import('./features/editor/components/upload-step/upload-step').then((c) => c.UploadStep),
+        title: 'Opifx | Homepage',
+        loadComponent: () =>
+          import('./features/editor/components/upload-step/upload-step').then((c) => c.UploadStep),
       },
       {
         path: 'edit',
-        loadComponent: () => import('./features/editor/components/edit-step/container/edit-step').then((c) => c.EditStep),
+        title: 'Opifx | Atmosphere & Adjustments',
+        loadComponent: () =>
+          import('./features/editor/components/edit-step/container/edit-step').then(
+            (c) => c.EditStep,
+          ),
       },
       {
         path: 'export',
-        loadComponent: () => import('./features/editor/components/export-step/container/export-step-container').then((c) => c.ExportStepContainer),
-      }
-    ]
+        title: 'Opifx | Render & Finalize',
+        loadComponent: () =>
+          import('./features/editor/components/export-step/container/export-step-container').then(
+            (c) => c.ExportStepContainer,
+          ),
+      },
+    ],
   },
   {
     path: '**',
